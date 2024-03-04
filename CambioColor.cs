0cf2302798d3a534c6950a6f635ed15f523babb4
diegocaballero <dicaball@ucm.es>
2024-03-04 12:22:59 +0100

    Cambio color

---
+++ CambioColor.cs
@@ -0,0 +1,14 @@
+﻿namespace AlgsDeOrdenación
+{
+    internal class CambioColor
+    {
+        public CambioColor()
+        {
+        }
+
+        public void CambiaColor(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+        }
+    }
+}
